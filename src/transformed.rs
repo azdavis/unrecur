79d6ecef87f69ac1cd8b90936492d0167f73fb51
Ariel Davis <ariel.z.davis@icloud.com>
2023-01-26 23:32:58 -0800

    Swap gunc

--- src/transformed.rs
+++ src/transformed.rs
@@ -9,32 +9,10 @@ pub fn func(es: &mut Vec<Event>, data: Data) -> usize {
 }
 
 pub fn gunc(es: &mut Vec<Event>, data: &mut Data) {
-  if data.num >= THRESHOLD {
-    es.push(Event::D(data.cond));
-  } else {
-    data.cond = !data.cond || es.len() % 7 == 0;
-    if es.len() < 5 || data.cond {
-      es.push(Event::E(es.len()));
-      let tmp = func(es, *data);
-      data.num = tmp + 3;
-    } else {
-      let mut cond = es.len() % 3 > 0;
-      if cond {
-        let tmp = func(es, *data);
-        cond = tmp % 2 == 0;
-      }
-      if cond {
-        es.push(Event::F);
-        data.num += 4;
-        gunc(es, data);
-        data.num += 6;
-      } else {
-        es.push(Event::G);
-        data.num += 3;
-        gunc(es, data);
-        data.num += 2;
-      }
-    }
+  let tmp = hunc(HuncArg::Gunc(es, data));
+  match tmp {
+    HuncRet::Func(_) => unreachable!(),
+    HuncRet::Gunc => {}
   }
 }
 
@@ -71,7 +49,33 @@ fn hunc(arg: HuncArg<'_>) -> HuncRet {
       HuncRet::Func(tmp)
     }
     HuncArg::Gunc(es, data) => {
-      gunc(es, data);
+      if data.num >= THRESHOLD {
+        es.push(Event::D(data.cond));
+      } else {
+        data.cond = !data.cond || es.len() % 7 == 0;
+        if es.len() < 5 || data.cond {
+          es.push(Event::E(es.len()));
+          let tmp = func(es, *data);
+          data.num = tmp + 3;
+        } else {
+          let mut cond = es.len() % 3 > 0;
+          if cond {
+            let tmp = func(es, *data);
+            cond = tmp % 2 == 0;
+          }
+          if cond {
+            es.push(Event::F);
+            data.num += 4;
+            gunc(es, data);
+            data.num += 6;
+          } else {
+            es.push(Event::G);
+            data.num += 3;
+            gunc(es, data);
+            data.num += 2;
+          }
+        }
+      }
       HuncRet::Gunc
     }
   }
