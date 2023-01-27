bf0fe3b3a74f352879e62dd7e7a4c114885a949d
Ariel Davis <ariel.z.davis@icloud.com>
2023-01-26 23:04:50 -0800

    Avoid early return

--- src/transformed.rs
+++ src/transformed.rs
@@ -3,48 +3,49 @@ use crate::common::{Data, Event, THRESHOLD};
 pub fn func(es: &mut Vec<Event>, mut data: Data) -> usize {
   if data.num >= THRESHOLD {
     es.push(Event::A(data.cond));
-    return es.len() + data.num;
-  }
-  data.cond = !data.cond || es.len() % 5 == 0;
-  if data.cond {
-    es.push(Event::B(data.num));
-    data.num += 1;
-    gunc(es, &mut data);
-    data.num + 2
+    es.len() + data.num
   } else {
-    es.push(Event::C);
-    data.num += 6;
-    let tmp = func(es, data);
-    tmp + 5
+    data.cond = !data.cond || es.len() % 5 == 0;
+    if data.cond {
+      es.push(Event::B(data.num));
+      data.num += 1;
+      gunc(es, &mut data);
+      data.num + 2
+    } else {
+      es.push(Event::C);
+      data.num += 6;
+      let tmp = func(es, data);
+      tmp + 5
+    }
   }
 }
 
 pub fn gunc(es: &mut Vec<Event>, data: &mut Data) {
   if data.num >= THRESHOLD {
     es.push(Event::D(data.cond));
-    return;
-  }
-  data.cond = !data.cond || es.len() % 7 == 0;
-  if es.len() < 5 || data.cond {
-    es.push(Event::E(es.len()));
-    let tmp = func(es, *data);
-    data.num = tmp + 3;
   } else {
-    let mut cond = es.len() % 3 > 0;
-    if cond {
+    data.cond = !data.cond || es.len() % 7 == 0;
+    if es.len() < 5 || data.cond {
+      es.push(Event::E(es.len()));
       let tmp = func(es, *data);
-      cond = tmp % 2 == 0;
-    }
-    if cond {
-      es.push(Event::F);
-      data.num += 4;
-      gunc(es, data);
-      data.num += 6;
+      data.num = tmp + 3;
     } else {
-      es.push(Event::G);
-      data.num += 3;
-      gunc(es, data);
-      data.num += 2;
+      let mut cond = es.len() % 3 > 0;
+      if cond {
+        let tmp = func(es, *data);
+        cond = tmp % 2 == 0;
+      }
+      if cond {
+        es.push(Event::F);
+        data.num += 4;
+        gunc(es, data);
+        data.num += 6;
+      } else {
+        es.push(Event::G);
+        data.num += 3;
+        gunc(es, data);
+        data.num += 2;
+      }
     }
   }
 }
