6354157a17abca3a513fe18c5dcd7e906c1494a7
Ariel Davis <ariel.z.davis@icloud.com>
2023-01-27 02:29:06 -0800

    Swap gunc

--- src/transformed.rs
+++ src/transformed.rs
@@ -8,35 +8,9 @@ pub fn func(es: &mut Vec<Event>, data: Data) -> usize {
 }
 
 pub fn gunc(es: &mut Vec<Event>, num: usize) -> Data {
-  let cond = es.len() % 2 == 0;
-  if num >= THRESHOLD {
-    es.push(Event::D);
-    Data { num: es.len() | num, cond }
-  } else {
-    let data = Data { num: num + 2, cond };
-    if es.len() < 5 {
-      es.push(Event::E(es.len()));
-      let tmp = func(es, data);
-      Data { num: tmp + 3, cond }
-    } else {
-      let mut cond = es.len() % 3 > 0;
-      if cond {
-        let tmp = func(es, data.clone());
-        cond = tmp % 2 == 0;
-      }
-      if cond {
-        es.push(Event::F);
-        let mut tmp = gunc(es, num + 4);
-        tmp.cond = !tmp.cond;
-        tmp
-      } else {
-        es.push(Event::G);
-        let fst = func(es, data);
-        let mut tmp = gunc(es, fst);
-        tmp.num += fst;
-        tmp
-      }
-    }
+  match hunc(es, Arg::Gunc(num)) {
+    Ret::Func(_) => unreachable!(),
+    Ret::Gunc(ret) => ret,
   }
 }
 
@@ -69,6 +43,37 @@ fn hunc(es: &mut Vec<Event>, arg: Arg) -> Ret {
         tmp + 3
       }
     }),
-    Arg::Gunc(num) => Ret::Gunc(gunc(es, num)),
+    Arg::Gunc(num) => Ret::Gunc({
+      let cond = es.len() % 2 == 0;
+      if num >= THRESHOLD {
+        es.push(Event::D);
+        Data { num: es.len() | num, cond }
+      } else {
+        let data = Data { num: num + 2, cond };
+        if es.len() < 5 {
+          es.push(Event::E(es.len()));
+          let tmp = func(es, data);
+          Data { num: tmp + 3, cond }
+        } else {
+          let mut cond = es.len() % 3 > 0;
+          if cond {
+            let tmp = func(es, data.clone());
+            cond = tmp % 2 == 0;
+          }
+          if cond {
+            es.push(Event::F);
+            let mut tmp = gunc(es, num + 4);
+            tmp.cond = !tmp.cond;
+            tmp
+          } else {
+            es.push(Event::G);
+            let fst = func(es, data);
+            let mut tmp = gunc(es, fst);
+            tmp.num += fst;
+            tmp
+          }
+        }
+      }
+    }),
   }
 }
