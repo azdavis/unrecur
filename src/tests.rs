d19f57729a5827c7e220f7deb2ba7b88f6948870
Ariel Davis <ariel.z.davis@icloud.com>
2023-01-26 22:46:33 -0800

    Copy original to transformed, test

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,5 @@
-use crate::common::{Data, Event};
-use crate::original;
+use crate::common::{Data, Event, THRESHOLD};
+use crate::{original, transformed};
 
 fn run(f: fn(&mut Vec<Event>, Data) -> usize, num: usize) -> (usize, Vec<Event>) {
   let mut es = Vec::<Event>::new();
@@ -16,4 +16,14 @@ fn original_37() {
     [B(37), E(1), B(38), E(3), B(39), B(40), G, E(7), C, A(false), F, E(11), C, A(false)]
   );
   assert_eq!(ret, 90);
+}
+
+#[test]
+fn original_is_transformed() {
+  for num in 0usize..=THRESHOLD {
+    let (r0, e0) = run(original::func, num);
+    let (r1, e1) = run(transformed::func, num);
+    assert_eq!(r0, r1);
+    assert_eq!(e0, e1);
+  }
 }
