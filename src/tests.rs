e2527a1eaf4bc1c5a29572c901b7dd1a706127b0
Ariel Davis <ariel.z.davis@icloud.com>
2023-01-27 03:02:04 -0800

    Rm smoke test

--- src/tests.rs
+++ src/tests.rs
@@ -8,17 +8,6 @@ fn run(f: fn(&mut Vec<Event>, Data) -> usize, num: usize) -> (usize, Vec<Event>)
 }
 
 #[test]
-fn smoke() {
-  use Event::{A, B, C, D, E, F, G};
-  let (ret, es) = run(original::func, 35);
-  assert_eq!(
-    es,
-    [B(35), E(1), B(38), E(3), B(41), B(44), G, C, A(false), D, F, B(48), G, A(true), D, F, D]
-  );
-  assert_eq!(63, ret);
-}
-
-#[test]
 fn original_is_transformed() {
   for num in 0usize..=THRESHOLD {
     let (r0, e0) = run(original::func, num);
