85539a59ccf4094a28fa660fcf0e2ba9f714c760
Ariel Davis <ariel.z.davis@icloud.com>
2023-01-27 03:12:59 -0800

    Test original is faster

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,6 @@
 use crate::common::{Data, Event, THRESHOLD};
 use crate::{original, transformed};
+use std::time::{Duration, Instant};
 
 fn run(f: fn(&mut Vec<Event>, Data) -> usize, num: usize) -> (usize, Vec<Event>) {
   let mut es = Vec::<Event>::new();
@@ -15,4 +16,22 @@ fn original_is_transformed() {
     assert_eq!(r0, r1);
     assert_eq!(e0, e1);
   }
+}
+
+fn with_duration<F, T>(f: F) -> (T, Duration)
+where
+  F: FnOnce() -> T,
+{
+  let start = Instant::now();
+  let ret = f();
+  (ret, Instant::now().duration_since(start))
+}
+
+#[test]
+fn original_is_faster() {
+  let ((r0, e0), t0) = with_duration(|| run(original::func, 0));
+  let ((r1, e1), t1) = with_duration(|| run(transformed::func, 0));
+  assert_eq!(r0, r1);
+  assert_eq!(e0, e1);
+  assert!(t0 < t1);
 }
