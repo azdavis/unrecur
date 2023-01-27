74b1242ca585a4c23f646c9759b36ad1d0e36bdc
Ariel Davis <ariel.z.davis@icloud.com>
2023-01-27 02:40:33 -0800

    Wrap in loop

--- src/transformed.rs
+++ src/transformed.rs
@@ -38,61 +38,64 @@ enum Cont {}
 
 fn hunc(es: &mut Vec<Event>, arg: Arg) -> Ret {
   let mut cs = Vec::<Cont>::new();
-  let ret = match arg {
-    Arg::Func(mut data) => {
-      if data.num >= THRESHOLD {
-        es.push(Event::A(data.cond));
-        Ret::Func(es.len() + data.num)
-      } else {
-        data.cond = !data.cond;
-        if data.cond {
-          es.push(Event::B(data.num));
-          data.num += 1;
-          let tmp = hunc(es, Arg::Gunc(data.num)).unwrap_gunc().num;
-          Ret::Func(tmp + 2)
+  #[allow(clippy::never_loop)]
+  loop {
+    let ret = match arg {
+      Arg::Func(mut data) => {
+        if data.num >= THRESHOLD {
+          es.push(Event::A(data.cond));
+          Ret::Func(es.len() + data.num)
         } else {
-          es.push(Event::C);
-          data.num += 6;
-          let tmp = hunc(es, Arg::Func(data)).unwrap_func();
-          Ret::Func(tmp + 3)
-        }
-      }
-    }
-    Arg::Gunc(num) => {
-      let cond = es.len() % 2 == 0;
-      if num >= THRESHOLD {
-        es.push(Event::D);
-        Ret::Gunc(Data { num: es.len() | num, cond })
-      } else {
-        let data = Data { num: num + 2, cond };
-        if es.len() < 5 {
-          es.push(Event::E(es.len()));
-          let tmp = hunc(es, Arg::Func(data)).unwrap_func();
-          Ret::Gunc(Data { num: tmp + 3, cond })
-        } else {
-          let mut cond = es.len() % 3 > 0;
-          if cond {
-            let tmp = hunc(es, Arg::Func(data.clone())).unwrap_func();
-            cond = tmp % 2 == 0;
-          }
-          if cond {
-            es.push(Event::F);
-            let mut tmp = hunc(es, Arg::Gunc(num + 4)).unwrap_gunc();
-            tmp.cond = !tmp.cond;
-            Ret::Gunc(tmp)
+          data.cond = !data.cond;
+          if data.cond {
+            es.push(Event::B(data.num));
+            data.num += 1;
+            let tmp = hunc(es, Arg::Gunc(data.num)).unwrap_gunc().num;
+            Ret::Func(tmp + 2)
           } else {
-            es.push(Event::G);
-            let fst = hunc(es, Arg::Func(data)).unwrap_func();
-            let mut tmp = hunc(es, Arg::Gunc(fst)).unwrap_gunc();
-            tmp.num += fst;
-            Ret::Gunc(tmp)
+            es.push(Event::C);
+            data.num += 6;
+            let tmp = hunc(es, Arg::Func(data)).unwrap_func();
+            Ret::Func(tmp + 3)
           }
         }
       }
+      Arg::Gunc(num) => {
+        let cond = es.len() % 2 == 0;
+        if num >= THRESHOLD {
+          es.push(Event::D);
+          Ret::Gunc(Data { num: es.len() | num, cond })
+        } else {
+          let data = Data { num: num + 2, cond };
+          if es.len() < 5 {
+            es.push(Event::E(es.len()));
+            let tmp = hunc(es, Arg::Func(data)).unwrap_func();
+            Ret::Gunc(Data { num: tmp + 3, cond })
+          } else {
+            let mut cond = es.len() % 3 > 0;
+            if cond {
+              let tmp = hunc(es, Arg::Func(data.clone())).unwrap_func();
+              cond = tmp % 2 == 0;
+            }
+            if cond {
+              es.push(Event::F);
+              let mut tmp = hunc(es, Arg::Gunc(num + 4)).unwrap_gunc();
+              tmp.cond = !tmp.cond;
+              Ret::Gunc(tmp)
+            } else {
+              es.push(Event::G);
+              let fst = hunc(es, Arg::Func(data)).unwrap_func();
+              let mut tmp = hunc(es, Arg::Gunc(fst)).unwrap_gunc();
+              tmp.num += fst;
+              Ret::Gunc(tmp)
+            }
+          }
+        }
+      }
+    };
+    while let Some(cont) = cs.pop() {
+      match cont {}
     }
-  };
-  while let Some(cont) = cs.pop() {
-    match cont {}
+    return ret;
   }
-  ret
 }
