d1ad5a08263829893554130e315262c93e7bb24c
Ariel Davis <ariel.z.davis@icloud.com>
2023-01-27 02:27:20 -0800

    Avoid early return

--- src/transformed.rs
+++ src/transformed.rs
@@ -3,19 +3,20 @@ use crate::common::{Data, Event, THRESHOLD};
 pub fn func(es: &mut Vec<Event>, mut data: Data) -> usize {
   if data.num >= THRESHOLD {
     es.push(Event::A(data.cond));
-    return es.len() + data.num;
-  }
-  data.cond = !data.cond;
-  if data.cond {
-    es.push(Event::B(data.num));
-    data.num += 1;
-    let tmp = gunc(es, data.num).num;
-    tmp + 2
+    es.len() + data.num
   } else {
-    es.push(Event::C);
-    data.num += 6;
-    let tmp = func(es, data);
-    tmp + 3
+    data.cond = !data.cond;
+    if data.cond {
+      es.push(Event::B(data.num));
+      data.num += 1;
+      let tmp = gunc(es, data.num).num;
+      tmp + 2
+    } else {
+      es.push(Event::C);
+      data.num += 6;
+      let tmp = func(es, data);
+      tmp + 3
+    }
   }
 }
 
@@ -23,30 +24,31 @@ pub fn gunc(es: &mut Vec<Event>, num: usize) -> Data {
   let cond = es.len() % 2 == 0;
   if num >= THRESHOLD {
     es.push(Event::D);
-    return Data { num: es.len() | num, cond };
-  }
-  let data = Data { num: num + 2, cond };
-  if es.len() < 5 {
-    es.push(Event::E(es.len()));
-    let tmp = func(es, data);
-    Data { num: tmp + 3, cond }
+    Data { num: es.len() | num, cond }
   } else {
-    let mut cond = es.len() % 3 > 0;
-    if cond {
-      let tmp = func(es, data.clone());
-      cond = tmp % 2 == 0;
-    }
-    if cond {
-      es.push(Event::F);
-      let mut tmp = gunc(es, num + 4);
-      tmp.cond = !tmp.cond;
-      tmp
+    let data = Data { num: num + 2, cond };
+    if es.len() < 5 {
+      es.push(Event::E(es.len()));
+      let tmp = func(es, data);
+      Data { num: tmp + 3, cond }
     } else {
-      es.push(Event::G);
-      let fst = func(es, data);
-      let mut tmp = gunc(es, fst);
-      tmp.num += fst;
-      tmp
+      let mut cond = es.len() % 3 > 0;
+      if cond {
+        let tmp = func(es, data.clone());
+        cond = tmp % 2 == 0;
+      }
+      if cond {
+        es.push(Event::F);
+        let mut tmp = gunc(es, num + 4);
+        tmp.cond = !tmp.cond;
+        tmp
+      } else {
+        es.push(Event::G);
+        let fst = func(es, data);
+        let mut tmp = gunc(es, fst);
+        tmp.num += fst;
+        tmp
+      }
     }
   }
 }
